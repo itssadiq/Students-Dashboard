fbda5f9cc34cb4274b01f0f6db91ac91f1951587
itssadiq <sadiqalishaikh009@gmail.com>
2025-06-15 15:19:26 +0500

    Made changes to use basic form functionalities on empty fields etc.
    deploying to test the functionality.

--- scripts/signUp.js
+++ scripts/signUp.js
@@ -1,28 +1,26 @@
 import { saveSignUpDetailsToDB } from "../backend/database.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector(".sign-up-button")
-    .addEventListener("click", (event) => {
-      event.preventDefault();
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
 
-      const nameElement = document.querySelector(".full-name");
-      const emailElement = document.querySelector(".email");
-      const passwordElement = document.querySelector(".password");
+    const nameElement = document.querySelector(".full-name");
+    const emailElement = document.querySelector(".email");
+    const passwordElement = document.querySelector(".password");
 
-      const full_name = nameElement.value;
-      const email_address = emailElement.value;
-      const password = passwordElement.value;
+    const full_name = nameElement.value;
+    const email_address = emailElement.value;
+    const password = passwordElement.value;
 
-      if (full_name == "" || email_address == "" || password == "") {
-        alert("input fields cannot be empty");
-      } else {
-        try {
-          saveSignUpDetailsToDB(full_name, email_address, password);
-          window.location.href = "./studentsLogin.html";
-        } catch (error) {
-          console.error("error signing up", error.message);
-        }
-      }
-    });
+    // if (full_name == "" || email_address == "" || password == "") {
+    //   alert("input fields cannot be empty");
+    // } else {
+    try {
+      saveSignUpDetailsToDB(full_name, email_address, password);
+      window.location.href = "./studentsLogin.html";
+    } catch (error) {
+      console.error("error signing up", error.message);
+    }
+    // }
+  });
 });
